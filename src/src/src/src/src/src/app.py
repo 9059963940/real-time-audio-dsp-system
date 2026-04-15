3650dba0d079fe5f5d5b96517394d22fd8a07e2f
Seelam Deepika <149002267+9059963940@users.noreply.github.com>
2026-04-15 18:52:52 +0530

    Update app.py

--- src/src/src/src/src/src/app.py
+++ src/src/src/src/src/src/app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+from realtime import process_realtime
 
+st.title("🎧 Real-Time Audio DSP System")
+
+if st.button("Start Recording"):
+    original, filtered, fs = process_realtime()
+    
+    st.write("Processing complete")
+    st.line_chart(original)
+    st.line_chart(filtered)
