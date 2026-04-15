b657d38b0f4be395817fb796393a7f751e1dbf80
Seelam Deepika <149002267+9059963940@users.noreply.github.com>
2026-04-15 18:52:12 +0530

    Update visualization.py

--- src/src/src/src/src/src/visualization.py
+++ src/src/src/src/src/src/visualization.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+def plot_signals(original, filtered):
+    plt.figure(figsize=(10,5))
+
+    plt.subplot(2,1,1)
+    plt.title("Original Signal")
+    plt.plot(original)
+
+    plt.subplot(2,1,2)
+    plt.title("Filtered Signal")
+    plt.plot(filtered)
+
+    plt.tight_layout()
+    plt.show()
