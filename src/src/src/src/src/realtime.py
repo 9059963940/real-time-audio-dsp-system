ba5e4d696ea3749098ce7e920cde3d38f344ce48
Seelam Deepika <149002267+9059963940@users.noreply.github.com>
2026-04-15 18:51:49 +0530

    Update realtime.py

--- src/src/src/src/src/realtime.py
+++ src/src/src/src/src/realtime.py
@@ -1,1 +1,7 @@
+from audio_io import record_audio
+from noise_filter import spectral_subtraction
 
+def process_realtime():
+    signal, fs = record_audio()
+    filtered = spectral_subtraction(signal, fs)
+    return signal, filtered, fs
