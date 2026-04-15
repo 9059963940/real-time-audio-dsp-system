1f6664974ff14ec3324f246806177c822bd3b153
Seelam Deepika <149002267+9059963940@users.noreply.github.com>
2026-04-15 18:50:52 +0530

    Update audio_io.py

--- src/src/audio_io.py
+++ src/src/audio_io.py
@@ -1,1 +1,12 @@
+import sounddevice as sd
+import soundfile as sf
 
+def record_audio(duration=5, fs=44100):
+    print("Recording...")
+    audio = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+    sd.wait()
+    print("Recording complete")
+    return audio.flatten(), fs
+
+def save_audio(file_path, data, samplerate):
+    sf.write(file_path, data, samplerate)
